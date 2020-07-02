4681cc42d68092a6fcd6288b7db699d0f2423b4e
Rafał Kuć <r.kuc@solr.pl>
2020-07-02 12:08:55 +0200

    Updates to Log4J 2 examples

---
+++ log4j2/src/main/java/com/sematext/blog/logging/Log4JExceptionNoThrowable.java
@@ -0,0 +1,18 @@
+package com.sematext.blog.logging;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+public class Log4JExceptionNoThrowable {
+  private static final Logger LOGGER = LogManager.getLogger(Log4JExceptionNoThrowable.class);
+
+  public static void main(String[] args) {
+    try {
+      throw new IOException("This is an I/O error");
+    } catch (IOException ioe) {
+      LOGGER.error("Error while executing main thread");
+    }
+  }
+}
